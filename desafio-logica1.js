82be3a82b0dcaf1e86c9586f3fba63a21d96c871
EnzoGabrielCode <enzokof2005@gmail.com>
2024-10-07 19:06:54 -0300

    concluido

---
+++ desafio-logica1.js
@@ -0,0 +1,20 @@
+let nomeDoHeroi = "Shawlin_matador_de_porco";
+let XP = 10000000000000;
+
+if(XP <= 1000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ferro.")
+}else if(XP <= 2000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Bronze.")
+}else if(XP <= 5000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Prata.")
+}else if(XP <= 7000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ouro.")
+}else if(XP <= 8000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Platina.")
+}else if(XP <= 9000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Ascendente.")
+}else if(XP <= 10000){
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Imortal.")
+}else{
+    console.log("O Herói de nome " + nomeDoHeroi + " está no nível de Radiante.")
+}
